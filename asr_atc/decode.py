2c3e11ac7bd8704466f5efe315cce898ff745b14
Thadeous Goodwyn <thad.goodwyn@gmail.com>
2021-08-23 21:09:51 -0400

    Accept a list.

--- asr_atc/decode.py
+++ asr_atc/decode.py
@@ -1,9 +1,9 @@
 from typing import Dict, List
 
 
-def prepare_vocab_for_decode(vocab_dict: Dict) -> List[str]:
+def prepare_vocab_for_decode(ordered_vocab: List) -> List[str]:
     # create a vocabulary expected by the ctc decoder
-    labels = [k for k, v in sorted(vocab_dict.items(), key=lambda item: item[1])]
+    labels = [k for k, v in ordered_vocab]
     labels = [k if k != '|' else ' ' for k in labels]
     labels = [k if k != '<s>' else '<' for k in labels]
     labels = [k if k != '</s>' else '>' for k in labels]
